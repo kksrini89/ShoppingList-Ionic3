5bbc998ccbf3ab4e801c1042f3ce64ce36d352a5
Srinivasan <kksrini89@gmail.com>
2017-08-28 08:33:22 +0530

    Initial commit with add shopping is done

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const Firebase_Credentials = {
+    apiKey: "AIzaSyBANZGwMmiobpsKylAeuEPV5BCUJ_XzNKc",
+    authDomain: "angular2-api-service.firebaseapp.com",
+    databaseURL: "https://angular2-api-service.firebaseio.com",
+    projectId: "angular2-api-service",
+    storageBucket: "angular2-api-service.appspot.com",
+    messagingSenderId: "320771185578"
+}
